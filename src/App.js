54caddd3647588e8747b7573cc1d9728861c7258
Babatunde Abdulhameed Olakunle <humiditii45@gmail.com>
2020-08-12 07:29:35 +0000

    adding about page

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import Layout from './Layout/Layout';
 import home from './Components/Pages/Home/Home';
+import about from './Components/Pages/About/About';
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <div className="App-header">
           <Switch>
               <Route path='/' exact component={home} />
+              <Route path='/about' component={about} />
           </Switch>
         </div>
           
---
+++ src/Components/Pages/About/About.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './about.css';
+
+const about = (props) => {
+    return (
+        <div>
+            <div    class="write-up">
+                <h1 class="headings"> ABout Us.</h1>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sodales mauris lorem, in rhoncus metus elementum et. Aenean nec mattis diam, in molestie massa. Etiam pharetra risus at lorem dapibus, aliquet eleifend neque consectetur. Curabitur ac libero in ligula condimentum tincidunt. Cras egestas lacinia efficitur. Fusce ornare ex sapien, non feugiat augue semper vitae. Donec tristique sit amet libero at dapibus. Nullam tincidunt imperdiet lectus sit amet mollis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+            </div>
+
+            <div class="bottom">
+                <div class="write-up">
+                    <h1 class="headings ">DSN BOOTCAMP Visualizer</h1>                                                                                            
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sodales mauris lorem, in rhoncus metus elementum et. Aenean nec mattis diam, in molestie massa. Etiam pharetra risus at lorem dapibus, aliquet eleifend neque consectetur. Curabitur ac libero in ligula condimentum tincidunt. Cras egestas lacinia efficitur. Fusce ornare ex sapien, non feugiat augue semper vitae. Donec tristique sit amet libero at dapibus. Nullam tincidunt imperdiet lectus sit amet mollis. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.
+                </div>
+        </div>
+        </div>
+    )
+}
+
+export default about
